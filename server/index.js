941a61a8a2d1930db672698fd2889ca7e73af002
Debanjan Mukherjee <debanjanmukherjee015@gmail.com>
2024-03-27 22:11:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ import { RecipeRouter } from "./controllers/Recipes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin : ["https://recipe-view-backend.vercel.app"],
+    methods : ["GET", "POST" ,"PUT"],
+    credentials :true
+}));
 app.use(express.json());
 
 const db = mongoose.connect("mongodb+srv://mukherjeed681:test@cluster0.2yulhdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
